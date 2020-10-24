7cd1b5ff6fe74fedf7058b161cc47c43f2d21310
Ali <aifares@buffalo.edu>
2020-10-24 14:06:10 -0400

    Model and DataBase

---
+++ UBHacking2020/EasyOrder/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EasyorderConfig(AppConfig):
+    name = 'EasyOrder'
---
+++ UBHacking2020/EasyOrder/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Restaurant(models.Model):
+    RestaurantCode = models.CharField(max_length=100)
+    RestaurantName = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.RestaurantName
+
+
+class Product(models.Model):
+    Restaurant = models.ForeignKey(Restaurant,on_delete=models.PROTECT,default=None)
+    Price = models.DecimalField(max_digits=5,decimal_places=4,blank=True, null=True)
+    Items = models.CharField(max_length=100,blank=True, null=True)
+    Description = models.CharField(max_length=1000,blank=True, null=True)
+    Image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return "%s, %s, %s" %(self.Items, self.Price, self.Description)
+
+
+
+
+
+
+
+
+# Create your models here.

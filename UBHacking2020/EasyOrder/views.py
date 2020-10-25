e0fd46fcf38a14b32a17417e4761aefc14daf87a
Ali <aifares@buffalo.edu>
2020-10-24 23:47:57 -0400

    Cart

--- UBHacking2020/EasyOrder/views.py
+++ UBHacking2020/EasyOrder/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import RestaurantsCode
-from .models import Restaurant , Product
+from .models import Restaurant , Product,Cart,CartItem
 
 
 
@@ -8,37 +8,62 @@ from .models import Restaurant , Product
 # Create your views here.
 
 def home(request):
-    print(request.POST)
     form = RestaurantsCode(request.POST or None)
-
     context= {"form":form}
     template= "EasyOrder/home.html"
     if request.method == 'POST':
+        if 'code' in request.POST:
+            code = request.POST['code']
+            print(code)
+            if form.is_valid():
+                ResObject =  Restaurant.objects.get(RestaurantCode = code)
+                products = Product.objects.filter(Restaurant = ResObject)
+                context= {"form":form, "ResObject" : ResObject, "products":products}
+                return render(request, "EasyOrder/menu.html", context)
 
-        code = request.POST['code']
-        print(code)
-        if form.is_valid():
-            ResObject =  Restaurant.objects.get(RestaurantCode = code)
-            products = Product.objects.filter(Restaurant = ResObject)
-            context= {"form":form, "ResObject" : ResObject, "products":products}
+
+
+
+        if "menu" in request.POST:
+            carList= []
+            print(request.POST)
+            print(request.POST['name1'])
+            carList.append(request.POST['name1'])
+            print(carList)
 
 
 
 
-            return render(request, "EasyOrder/menu.html", context)
-    else:
-        return render(request, template, context)
+
+
+
+
+
 
     context= {"form":form}
 
     return render(request, template, context)
 
 def menu(request):
-    context={}
-    template= "EasyOrder/menu.html"
-    return render(request, template, context)
+    return render(request, "EasyOrder/checkout.html", context)
 
-def checkout(request):
+
+def update_cart(request,slug):
+    cart = Cart.objects.all()[0]
+    try:
+        product = Product.objects.get(slug=slug)
+    except Product.DoesNotExist:
+        pass
+    except:
+        pass
+    if not product in cart.products.all():
+        cart.products.add(product)
+
+    return redirect(request.META['HTTP_REFERER'])
+
+
+
+def checkout(request,slug):
     context={}
     template= "EasyOrder/checkout.html"
     return render(request, template, context)
